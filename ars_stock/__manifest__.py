e64bf6e63857bda1cf54adadfef69d076dc37c2b
yudiw21 <yudi@arista-group.co.id>
2021-12-07 17:02:21 +0700

    [ADD] Ars_Stock

---
+++ ars_stock/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Inherit stock',
+    'category': 'Hidden',
+    'version': '1.0',
+    'summary': 'Penambahan Field ke stock',
+    'description':
+        """
+======================== ****
+stock FOR ARISTA GROUP    ****
+======================== ****
+        """,
+    'depends': ['stock'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/sale_ttums_views.xml',
+        # 'views/hr_skill_views.xml'
+        ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': False,
+}
---
+++ ars_stock/models/stock.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import fields, models
+
+class SaleTTUMS(models.Model):
+    _name = "sale.ttums"
+
+    name = fields.Char("catatan")
+
